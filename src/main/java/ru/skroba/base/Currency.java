7e89a8d3748228b285a4bc049911865dd129c3f4
”Dmitri <dmitri.zd1958@gmail.com>
2023-03-08 15:24:35 +0100

    Add curencys

---
+++ src/main/java/ru/skroba/base/Currency.java
@@ -0,0 +1,24 @@
+package ru.skroba.base;
+
+import java.util.Arrays;
+
+public enum Currency {
+    RUB("rub"), EUR("eu"), USD("usd"), GBP("gbp");
+    
+    private final String name;
+    
+    Currency(final String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Currency getCurrencyByName(String name) {
+        return Arrays.stream(Currency.values())
+                .filter(it -> it.name.equals(name))
+                .findFirst()
+                .orElse(USD);
+    }
+}
